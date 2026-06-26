67185f2ed088d75c6dbfc3c3d93ee55276e5ab8d
gunforawhile <ainaa.arissa18@gmail.com>
2026-06-26 12:13:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,21 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Software Requirement Specification for Security-Related Requirements')
 
 st.info('The system is to identify security-related requirements in the SRS')
 
+st.info('Text / File Upload')
+
+st.write('Text')
 txt=st.text_area(
-  "Text to identify requirements"
+  "Text to identify the requirements"
 )
 
 st.write(f" {len(txt)} characters | {len(txt.split())} words")
+
+st.write('File upload')
+#support csv and pdf, multiple files
+uploaded_file=st.file_uploader(
+  "Upload file", accept_multiple_files=True,type=["csv","pdf"]
+)
