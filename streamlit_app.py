39a1fdddf4b63aac49451148e79df5553adaf7dc
gunforawhile <ainaa.arissa18@gmail.com>
2026-06-26 11:57:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title('Software Requirement Specification for Security-Related Requirements')
 
 st.info('The system is to identify security-related requirements in the SRS')
+
+txt=st.text_area(
+  "Text to identify requirements"
+)
+
+st.write(f" {len(txt)} characters are inserted.")
