3b72b780b58a108e4179b8c9d11773305deadd95
gunforawhile <ainaa.arissa18@gmail.com>
2026-06-26 12:31:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,20 @@ st.write(f" {len(txt)} characters | {len(txt.split())} words")
 st.write('File upload')
 #support csv and pdf, multiple files
 uploaded_file=st.file_uploader(
-  "Choose file(s)", accept_multiple_files=True,type=["csv","pdf"]
+  "Choose file(s)", accept_multiple_files=True,type=["csv","txt"]
 )
 if uploaded_file is not None:
-  #if text
-  stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-  string_data = stringio.read()
-  #if csv file
-  dataframe=pd.read_csv(uploaded_file)
+  
+  for uploaded_files in uploaded_file:
+    st.write(f"{uploaded_file.name}")
+    #if text
+    if uploaded_file.name.endswith(".csv"):
+        dataframe=pd.read_csv(uploaded_file)
+        string_data=df.to_string(index=False)
+  
+    #if csv file
+    elif uploaded_file.name.endswith(".txt"):
+      string_data=uploaded_file.read().decode("utf-8")
+      st.text_area("Text preview",string_data[:500] + "...", height=150)
+
+    all_text.append(string_data)
