91d47347d7a62c37adbc708b3e90fd2aafefa6b5
gunforawhile <ainaa.arissa18@gmail.com>
2026-06-26 11:52:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 Software Requirement Specification for Security-Related Requirements')
+st.title('Software Requirement Specification for Security-Related Requirements')
 
 st.write('Hello world!')
